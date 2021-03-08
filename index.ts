00d1739deb2c675382c13f19a8d544f26d439012
Peter Paul Elfferich <pp@exmachina.nl>
2021-03-08 18:53:14 +0100

    Order exports

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 /**
- * livery-interactive version.
+ * Livery Interactive version.
  */
 export const version = '__VERSION__';
 
 export { InteractiveBridge } from './src/InteractiveBridge';
-
-export { MockSdkBridge } from './src/MockSdkBridge';
-
 export { LiveryBridge } from './src/LiveryBridge';
+export { MockSdkBridge } from './src/MockSdkBridge';
