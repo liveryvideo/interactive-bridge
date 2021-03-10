fcaff528d1c71e87ddb66e16cda74b9b51ce8433
Peter Paul Elfferich <pp@exmachina.nl>
2021-03-10 07:58:15 +0100

    Patched demo rollup config to support the mock.html page besides the index.html page and to not generate a service worker.

--- rollup.config.demo.js
+++ rollup.config.demo.js
@@ -2,16 +2,17 @@ import { createSpaConfig } from '@open-wc/building-rollup';
 import merge from 'deepmerge';
 import cpy from 'rollup-plugin-cpy';
 
-const config = createSpaConfig({
-  injectServiceWorker: true,
-});
-
-export default merge(config, {
-  input: './demo/index.html',
-  plugins: [
-    cpy({
-      files: ['demo/*', '!demo/index.html'],
-      dest: 'dist',
-    }),
-  ],
-});
+export default [
+  merge(createSpaConfig({}), {
+    input: './demo/index.html',
+    plugins: [
+      cpy({
+        files: ['demo/*', '!demo/index.html', '!demo/mock.html'],
+        dest: 'dist',
+      }),
+    ],
+  }),
+  merge(createSpaConfig({}), {
+    input: './demo/mock.html',
+  }),
+];
