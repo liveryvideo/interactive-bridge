d2f3cd4480b4bc2866b9e3b619fa3dc85bb26046
Peter Paul Elfferich <pp@exmachina.nl>
2021-03-12 18:17:36 +0100

    Added source reference to semver.ts

--- src/util/semver.ts
+++ src/util/semver.ts
@@ -1,3 +1,4 @@
+// Source: https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
 const regex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
 
 export function semver(version: string) {
