8681653a8d46b891e581f671f7e6392775a3a889
Peter Paul Elfferich <pp@exmachina.nl>
2021-03-19 13:21:51 +0100

    build: replace __VERSION__ by package version in demo build

--- rollup.config.demo.js
+++ rollup.config.demo.js
@@ -1,11 +1,16 @@
 import { createSpaConfig } from '@open-wc/building-rollup';
+import replace from '@rollup/plugin-replace';
 import merge from 'deepmerge';
 import cpy from 'rollup-plugin-cpy';
+import pkg from './package.json';
 
 export default [
   merge(createSpaConfig({}), {
     input: './demo/index.html',
     plugins: [
+      replace({
+        __VERSION__: pkg.version,
+      }),
       cpy({
         files: ['demo/*', '!demo/index.html', '!demo/mock.html'],
         dest: 'dist',
@@ -14,5 +19,10 @@ export default [
   }),
   merge(createSpaConfig({}), {
     input: './demo/mock.html',
+    plugins: [
+      replace({
+        __VERSION__: pkg.version,
+      }),
+    ],
   }),
 ];
