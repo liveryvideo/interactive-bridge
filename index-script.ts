3805a6fc7e34beb896d72a8d9124ffa6a2bfb84e
Peter Paul Elfferich <pp@exmachina.nl>
2023-07-18 13:23:13 +0200

    feat: extract LiveryInteractive test element out of test page

--- index-script.ts
+++ index-script.ts
@@ -16,6 +16,8 @@ const params = new URLSearchParams(window.location.search);
 
 if (params.has('mock')) {
   window.mockPlayerBridge = new MockPlayerBridge();
+  // TODO: Also mock with player screenshot background and with PlayerBridge log like on mock.html page
+  // E.g: merge mock.html page into this?
 }
 
 customElements.define('livery-interactive', LiveryInteractive);
@@ -26,102 +28,3 @@ liveryInteractive.onload = () => {
   window.interactiveBridge = liveryInteractive.interactiveBridge;
 };
 document.body.appendChild(liveryInteractive);
-
-/*
-import { InteractiveBridge, MockPlayerBridge } from './index';
-import { stringify } from './src/util/stringify';
-
-// TODO: Refactor into <livery-testbridge> lit element
-
-function $<K extends keyof HTMLElementTagNameMap>(
-  selector: K,
-): HTMLElementTagNameMap[K];
-function $<E extends HTMLElement = HTMLElement>(selector: string): E;
-function $(selector: string) {
-  const element = document.querySelector(selector);
-  if (!element) {
-    throw new Error(`Element not found with selector: ${selector}`);
-  }
-  return element;
-}
-
-function createSetText(selector: string) {
-  return (value: unknown) => {
-    $(selector).innerText =
-      value instanceof Error ? value.toString() : stringify(value);
-  };
-}
-
-const params = new URLSearchParams(window.location.search);
-
-$('#version').innerText = __VERSION__;
-
-if (params.has('mock')) {
-  window.mockBridge = new MockPlayerBridge();
-}
-
-// Note: Don't use origin '*' like we do here unless security is not an issue for your purposes
-const bridge = new InteractiveBridge(window.mockBridge || '*');
-
-(
-  [
-    ['app-name', 'getAppName'],
-    ['customer-id', 'getCustomerId'],
-    ['endpoint-id', 'getEndpointId'],
-    ['latency', 'getLatency'],
-    ['livery-params', 'getLiveryParams'],
-    ['player-version', 'getPlayerVersion'],
-    ['stream-id', 'getStreamId'],
-  ] as const
-).forEach(([id, method]) => {
-  $(`#get-${id}`).addEventListener('click', () => {
-    const setText = createSetText(`#${id}`);
-    bridge[method]().then(setText, setText);
-  });
-});
-
-(
-  [
-    ['fullscreen', 'subscribeFullscreen'],
-    ['orientation', 'subscribeOrientation'],
-    ['quality', 'subscribeQuality'],
-    ['stream-phase', 'subscribeStreamPhase'],
-  ] as const
-).forEach(([id, method]) => {
-  $(`#subscribe-${id}`).addEventListener('click', () => {
-    const setText = createSetText(`#${id}`);
-    bridge[method](setText).then(setText, setText);
-  });
-});
-
-$('#player-command-form').addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const data = new FormData($<HTMLFormElement>('#player-command-form'));
-  const name = data.get('name') as string;
-  const argStr = data.get('arg') as string;
-  const arg: unknown = !argStr ? undefined : JSON.parse(argStr);
-
-  const setText = createSetText('#player-command-value');
-  bridge.sendPlayerCommand(name, arg, setText).then(setText, setText);
-});
-
-bridge.registerInteractiveCommand('test', (arg, handler) => {
-  const argStr = stringify(arg);
-  $('#interactive-command-arg').innerText = argStr;
-
-  window.setTimeout(() => handler(`${argStr}-result-2`), 2000);
-  window.setTimeout(() => handler(`${argStr}-result-3`), 4000);
-
-  return `${argStr}-result-1`;
-});
-
-declare global {
-  interface Window {
-    bridge: InteractiveBridge;
-    mockBridge?: MockPlayerBridge;
-  }
-}
-
-window.bridge = bridge;
-*/
--- src/livery-interactive/LiveryInteractive.ts
+++ src/livery-interactive/LiveryInteractive.ts
@@ -1,8 +1,38 @@
 import { css, html, LitElement } from 'lit';
-import { property } from 'lit/decorators.js';
+import { property, state } from 'lit/decorators.js';
 import type { AbstractPlayerBridge } from '../AbstractPlayerBridge';
 import { InteractiveBridge } from '../InteractiveBridge';
 import '../livery-bridge-log/LiveryBridgeLog';
+import { stringify } from '../util/stringify';
+
+const BRIDGE_GET_NAMES = [
+  'getAppName',
+  'getCustomerId',
+  'getEndpointId',
+  'getLatency',
+  'getLiveryParams',
+  'getPlayerVersion',
+  'getStreamId',
+] as const;
+const BRIDGE_SUBSCRIBE_NAMES = [
+  'subscribeFullscreen',
+  'subscribeOrientation',
+  'subscribeQuality',
+  'subscribeStreamPhase',
+] as const;
+
+type BridgeGetName = (typeof BRIDGE_GET_NAMES)[number];
+type BridgeSubscribeName = (typeof BRIDGE_SUBSCRIBE_NAMES)[number];
+
+function isBridgeGetMethodName(name: string): name is BridgeGetName {
+  return (BRIDGE_GET_NAMES as unknown as string[]).includes(name);
+}
+
+function isBridgeSubscribeMethodName(
+  name: string,
+): name is BridgeSubscribeName {
+  return (BRIDGE_SUBSCRIBE_NAMES as unknown as string[]).includes(name);
+}
 
 /**
  * Element which can be registered as 'livery-interactive' to be used as Livery Interactive element
@@ -10,9 +40,17 @@ import '../livery-bridge-log/LiveryBridgeLog';
  *
  * Note: This does not define itself as custom element 'livery-interactive' to avoid unintended conflicts!
  *
- * Usage: `customElements.define('livery-interactive', LiveryInteractive);`
- *
  * This dispatches a 'load' event once it's loaded and `interactiveBridge` has been assigned.
+ *
+ * @example
+ * customElements.define('livery-interactive', LiveryInteractive);
+ * const interactive = document.createElement('livery-interactive');
+ * // Assign player bridge instance when you are embedded as interactive element (not page) by player
+ * interactive.playerBridge = playerBridge;
+ * interactive.onload = () => {
+ *   // Use interactive.interactiveBridge
+ * };
+ * document.body.appendChild(interactive);
  */
 export class LiveryInteractive extends LitElement {
   static override readonly styles = css`
@@ -79,22 +117,47 @@ export class LiveryInteractive extends LitElement {
 
   /**
    * `PlayerBridge` to use as target for `interactiveBridge`.
-   * If undefined this will default using parent as target window and '*' as target origin.
    *
    * Note: Only effective when defined before first DOM connect.
+   *
+   * If `undefined` this will default to using `parent` as `target.window` and `'*'` as `target.origin`.
+   *
+   * Note: Without `origin` restriction don't send sensitive information or trust responses,
+   * this is only meant to be used for testing purposes.
    */
   playerBridge?: AbstractPlayerBridge;
+
+  @state()
+  private interactiveCommandArg = '';
+
+  @state()
+  private playerCommandValue = '';
 
   override connectedCallback() {
     super.connectedCallback();
 
     if (!this.interactiveBridge) {
       this.interactiveBridge = new InteractiveBridge(this.playerBridge ?? '*');
+
+      this.interactiveBridge.registerInteractiveCommand(
+        'test',
+        (arg, handler) => {
+          const argStr = stringify(arg);
+          this.interactiveCommandArg = argStr;
+
+          window.setTimeout(() => handler(`${argStr}-result-2`), 2000);
+          window.setTimeout(() => handler(`${argStr}-result-3`), 4000);
+
+          return `${argStr}-result-1`;
+        },
+      );
+
       this.dispatchEvent(new Event('load'));
     }
   }
 
   override render() {
+    /* eslint-disable @typescript-eslint/unbound-method -- lit html handles event listener binding */
     return html`
       <div class="controls-space">
         <!-- Livery Player controls can be shown here -->
@@ -125,73 +188,83 @@ export class LiveryInteractive extends LitElement {
             </tr>
             <tr>
               <td>
-                <button id="get-app-name">getAppName</button>
+                <button @click=${this.handlePlayerGet}>getAppName</button>
               </td>
-              <td id="app-name"></td>
+              <td id="getAppNameOutput"></td>
             </tr>
             <tr>
               <td>
-                <button id="get-customer-id">getCustomerId</button>
+                <button @click=${this.handlePlayerGet}>getCustomerId</button>
               </td>
-              <td id="customer-id"></td>
+              <td id="getCustomerIdOutput"></td>
             </tr>
             <tr>
               <td>
-                <button id="get-endpoint-id">getEndpointId</button>
+                <button @click=${this.handlePlayerGet}>getEndpointId</button>
               </td>
-              <td id="endpoint-id"></td>
-            </tr>
-            <tr>
-              <td><button id="get-latency">getLatency</button></td>
-              <td id="latency"></td>
-            </tr>
-            <tr>
-              <td><button id="get-livery-params">getLiveryParams</button></td>
-              <td id="livery-params"></td>
+              <td id="getEndpointIdOutput"></td>
             </tr>
             <tr>
               <td>
-                <button id="get-player-version">getPlayerVersion</button>
+                <button @click=${this.handlePlayerGet}>getLatency</button>
               </td>
-              <td id="player-version"></td>
+              <td id="getLatencyOutput"></td>
             </tr>
             <tr>
               <td>
-                <button id="get-stream-id">getStreamId</button>
+                <button @click=${this.handlePlayerGet}>getLiveryParams</button>
               </td>
-              <td id="stream-id"></td>
+              <td id="getLiveryParamsOutput"></td>
             </tr>
             <tr>
               <td>
-                <button id="subscribe-fullscreen">subscribeFullscreen</button>
+                <button @click=${this.handlePlayerGet}>getPlayerVersion</button>
               </td>
-              <td id="fullscreen"></td>
+              <td id="getPlayerVersionOutput"></td>
             </tr>
             <tr>
               <td>
-                <button id="subscribe-orientation">subscribeOrientation</button>
+                <button @click=${this.handlePlayerGet}>getStreamId</button>
               </td>
-              <td id="orientation"></td>
+              <td id="getStreamIdOutput"></td>
             </tr>
             <tr>
               <td>
-                <button id="subscribe-quality">subscribeQuality</button>
+                <button @click=${this.handlePlayerSubscribe}>
+                  subscribeFullscreen
+                </button>
               </td>
-              <td id="quality"></td>
+              <td id="subscribeFullscreenOutput"></td>
             </tr>
             <tr>
               <td>
-                <button id="subscribe-stream-phase">
+                <button @click=${this.handlePlayerSubscribe}>
+                  subscribeOrientation
+                </button>
+              </td>
+              <td id="subscribeOrientationOutput"></td>
+            </tr>
+            <tr>
+              <td>
+                <button @click=${this.handlePlayerSubscribe}>
+                  subscribeQuality
+                </button>
+              </td>
+              <td id="subscribeQualityOutput"></td>
+            </tr>
+            <tr>
+              <td>
+                <button @click=${this.handlePlayerSubscribe}>
                   subscribeStreamPhase
                 </button>
               </td>
-              <td id="stream-phase"></td>
+              <td id="subscribeStreamPhaseOutput"></td>
             </tr>
           </table>
         </div>
 
         <div class="panel">
-          <form id="player-command-form" method="post" action="#">
+          <form method="post" action="#" @submit=${this.handlePlayerCommand}>
             <table>
               <tr>
                 <th colspan="2">Custom Player Command</th>
@@ -212,7 +285,7 @@ export class LiveryInteractive extends LitElement {
               </tr>
               <tr>
                 <th>Value:</th>
-                <td id="player-command-value"></td>
+                <td>${this.playerCommandValue}</td>
               </tr>
             </table>
           </form>
@@ -229,7 +302,7 @@ export class LiveryInteractive extends LitElement {
             </tr>
             <tr>
               <th>Arg:</th>
-              <td id="interactive-command-arg"></td>
+              <td>${this.interactiveCommandArg}</td>
             </tr>
           </table>
         </div>
@@ -247,5 +320,72 @@ export class LiveryInteractive extends LitElement {
         <!-- Livery Player controls can be shown here -->
       </div>
     `;
+    /* eslint-enable @typescript-eslint/unbound-method */
+  }
+
+  private createSetText(selector: string) {
+    const element = this.renderRoot.querySelector(selector);
+    if (!element || !(element instanceof HTMLElement)) {
+      throw new Error(`No HTMLElement found with selector: ${selector}`);
+    }
+
+    return (value: unknown) => {
+      element.innerText = stringify(value);
+    };
+  }
+
+  private handlePlayerCall(type: 'get' | 'subscribe', event: Event) {
+    if (!event.target || !(event.target instanceof HTMLElement)) {
+      throw new Error('Unsupported event target');
+    }
+    if (!this.interactiveBridge) {
+      throw new Error('interactiveBridge undefined');
+    }
+
+    const methodName = event.target.innerText.trim();
+    const setText = this.createSetText(`#${methodName}Output`);
+
+    if (type === 'get') {
+      if (!isBridgeGetMethodName(methodName)) {
+        throw new Error(`Invalid ${type} method name: ${methodName}`);
+      }
+      this.interactiveBridge[methodName]().then(setText, setText);
+    } else {
+      if (!isBridgeSubscribeMethodName(methodName)) {
+        throw new Error(`Invalid ${type} method name: ${methodName}`);
+      }
+      this.interactiveBridge[methodName](setText).then(setText, setText);
+    }
+  }
+
+  private handlePlayerCommand(event: Event) {
+    if (!event.target || !(event.target instanceof HTMLFormElement)) {
+      throw new Error('Unsupported event target');
+    }
+    if (!this.interactiveBridge) {
+      throw new Error('interactiveBridge undefined');
+    }
+
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+    const name = data.get('name') as string;
+    const argStr = data.get('arg') as string;
+    const arg: unknown = !argStr ? undefined : JSON.parse(argStr);
+
+    const setText = (value: unknown) => {
+      this.playerCommandValue = stringify(value);
+    };
+    this.interactiveBridge
+      .sendPlayerCommand(name, arg, setText)
+      .then(setText, setText);
+  }
+
+  private handlePlayerGet(event: Event) {
+    this.handlePlayerCall('get', event);
+  }
+
+  private handlePlayerSubscribe(event: Event) {
+    this.handlePlayerCall('subscribe', event);
   }
 }
