5cdd38888d4c4d02d93eea0afa6a386f841dc8e3
Peter Paul Elfferich <pp@exmachina.nl>
2024-01-29 16:21:45 +0100

    docs: update README and add package documentation for use of tsdocs.dev
    Ticket: https://exmachina.atlassian.net/browse/LIV-2773

--- index.ts
+++ index.ts
@@ -1,4 +1,21 @@
 /**
+ * Bridge for communicating between a Livery Video Player and the interactive layer page shown within that.
+ *
+ * @remarks
+ * The main part of this package you are likely to use is the {@link InteractiveBridge} class.
+ * For debugging you can also use the {@link LiveryBridgeLog}, {@link LiveryBridgeMock} and
+ * {@link LiveryBridgeInteractive} elements.
+ * There is also the {@link version} variable.
+ * And the helper classes used to implement these and the web player:
+ * {@link AbstractPlayerBridge}, {@link LiveryBridge} and {@link MockPlayerBridge}.
+ *
+ * **Note:** If you are reading this at tsdocs.dev we recommend you to disable the `Inherited`
+ * member visibility from `Settings` to make it easier to find our members.
+ *
+ * @packageDocumentation
+ */
+
+/**
  * Livery Interactive SDK version.
  */
 export const version = __VERSION__;
